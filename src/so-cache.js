04290b3da27bb9e9b58e08679dcdbe3b2de28736
bigwhiteshark <yangxinming@outlook.com>
2014-08-06 00:33:42 +0800

    improved cache plugin

--- src/so-cache.js
+++ src/so-cache.js
@@ -6,7 +6,7 @@
  */
 (function(global) {
     var storagePrefix = 'sojs-',
-        defaultExpiration = sojs.opts.expiration || 5000 * 60 * 60 * 1000,
+        defaultExpiration = sojs.opts.expiration || 5000 * 60 * 60 * 1000, //ms unit
         storage = sojs.opts.storage || global.localStorage;
 
     function wrapData(obj) {
@@ -17,6 +17,35 @@
     }
 
     var cache = {
+        add: function(key, storeObj) {
+            try {
+                storage.setItem(key, JSON.stringify(storeObj));
+                return true;
+            } catch (e) {
+                //On the iPhone/ipod sometimes setItem will appear QUOTA_EXCEEDED_ERR errors,
+                //then the general before setItem first removeItem to ok
+                if (e.name.toUpperCase().indexOf('QUOTA') >= 0) {
+                    var item, tempScripts = [];
+                    for (item in storage) {
+                        if (item.indexOf(storagePrefix) === 0) {
+                            tempScripts.push(JSON.parse(storage[item]));
+                        }
+                    }
+                    if (tempScripts.length) {
+                        tempScripts.sort(function(a, b) {
+                            return a.stamp - b.stamp;
+                        });
+                        cache.remove(tempScripts[0].key);
+                        return cache.add(key, storeObj);
+                    } else { // no files to remove. Larger than available quota
+                        return;
+                    }
+                } else { // some other error
+                    return;
+                }
+            }
+        },
+
         remove: function(key) {
             storage.removeItem(key);
             return this;
@@ -39,41 +68,7 @@
                     this.remove(key);
                 }
             }
-
             return this;
-        }
-    }
-
-    function addLocalStorage(key, storeObj) {
-        try {
-            storage.setItem(key, JSON.stringify(storeObj));
-            return true;
-        } catch (e) {
-            //On the iPhone/ipod sometimes setItem will appear QUOTA_EXCEEDED_ERR errors,
-            //then the general before setItem first removeItem to ok
-            if (e.name.toUpperCase().indexOf('QUOTA') >= 0) {
-                var item, tempScripts = [];
-
-                for (item in storage) {
-                    if (item.indexOf(storagePrefix) === 0) {
-                        tempScripts.push(JSON.parse(storage[item]));
-                    }
-                }
-
-                if (tempScripts.length) {
-                    tempScripts.sort(function(a, b) {
-                        return a.stamp - b.stamp;
-                    });
-                    cache.remove(tempScripts[0].key);
-                    return addLocalStorage(key, storeObj);
-
-                } else { // no files to remove. Larger than available quota
-                    return;
-                }
-
-            } else { // some other error
-                return;
-            }
         }
     };
 
@@ -86,14 +81,13 @@
         if (id.indexOf('sync') == -1) {
             if (!cache.get(key)) {
                 mod.once('exec', function() {
-                    //console.log(mod.factory);
                     var f = mod.factory;
                     f = typeof(f) === 'function' ? f + '' : f;
                     var storeData = {
                         factory: f
-                    }
+                    };
                     storeData = wrapData(storeData);
-                    addLocalStorage(key, storeData);
+                    cache.add(key, storeData);
                 })
             }
         }
@@ -106,12 +100,12 @@
         if (storeData) {
             mod.requested = true;
             var factory = storeData.factory;
-            if (/^function/img.test(factory)) { // factory is function,
-                factory = /\{((.|[\r\n\t\s])*)\}$/img.exec(factory)[0];
-                factory = new Function('require,exports,module', factory);
+            if (/^function/img.test(factory)) { // if factory is function
+                var args = /\((.*)\)/mg.exec(factory)[1]; //get factory arguments
+                factory = /\{((.|[\r\n\t\s])*)\}$/img.exec(factory)[1]; //get factory body
+                factory = new Function(args, factory);
             }
-            mod.onDefine(factory)
+            mod.onDefine(factory);
         }
-
     })
 })(this);
